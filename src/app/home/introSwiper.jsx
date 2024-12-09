31a0ef2bb88aa16f2c109909dfe919743abd2cee
odd:odd <eehd80yul@gmail.com>
2024-12-09 10:30:13 +0900

    swiper 적용

--- src/app/home/introSwiper.jsx
+++ src/app/home/introSwiper.jsx
@@ -12,13 +12,34 @@ import 'swiper/css/navigation';
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 import { Image } from '@chakra-ui/next-js';
-import { AspectRatio } from '@chakra-ui/react';
+import { AspectRatio, Container } from '@chakra-ui/react';
+
+const slideData = [
+  {
+    id: 1,
+    image: '/images/pattern/thumb/main-insight1.png',
+    title: 'AI를 일상에 적용하는 법: 대학생편',
+    description: '대학생들이 AI를 이용해 학습하는 방법에 대해 알아보자.',
+  },
+  {
+    id: 2,
+    image: '/images/pattern/thumb/main-insight2.png',
+    title: 'AI를 일상에 적용하는 법: 대학생편',
+    description: '대학생들이 AI를 이용해 학습하는 방법에 대해 알아보자.',
+  },
+  {
+    id: 3,
+    image: '/images/pattern/thumb/main-insight3.png',
+    title: 'AI를 일상에 적용하는 법: 대학생편',
+    description: '대학생들이 AI를 이용해 학습하는 방법에 대해 알아보자.',
+  },
+];
 
 export default function IntroSwiper() {
   return (
-    <>
+    <Container>
       <Swiper
-        slidesPerView={1}
+        slidesPerView={1.3}
         spaceBetween={30}
         loop={true}
         pagination={{
@@ -26,29 +47,20 @@ export default function IntroSwiper() {
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
-        className="mySwiper"
+        className="mySwiper !overflow-visible"
       >
-        <SwiperSlide>
-          <AspectRatio>
-            <Image src="/images/pattern/main/intro-slide-1.jpg" alt="Picture of the author" width={1200} height={500} />
-          </AspectRatio>
-        </SwiperSlide>
-        <SwiperSlide>
-          <AspectRatio>
-          <Image src="/images/pattern/main/intro-slide-1.jpg" alt="Picture of the author" width={1200} height={500} className='w-full' />
-          </AspectRatio>
-        </SwiperSlide>
-        <SwiperSlide>
-          <AspectRatio>
-            <Image src="/images/pattern/main/intro-slide-2.jpg" alt="Picture of the author" width={1200} height={500} />
-          </AspectRatio>
-        </SwiperSlide>
-        <SwiperSlide>
-          <AspectRatio>
-          <Image src="/images/pattern/main/intro-slide-1.jpg" alt="Picture of the author" width={1200} height={500} className='w-full' />
-          </AspectRatio>
-        </SwiperSlide>
+        {slideData.map((slide, index) => (
+          <SwiperSlide key={index} className='flex flex-col gap-5'>
+            <AspectRatio ratio={2/1}>
+              <Image src={slide.image} alt={slide.title} width={1200} height={500} className='rounded-lg' />
+            </AspectRatio>
+            <div className='pt-5 flex flex-col gap-2'>
+              <strong className='text-3xl'>{slide.title}</strong>
+              <p className='text-lg'>{slide.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </>
+    </Container>
   );
 }
