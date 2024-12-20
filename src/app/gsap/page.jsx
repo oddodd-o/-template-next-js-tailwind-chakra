b02f9dacf59fb98a25319c38238197d5d4a9cfaf
odd:odd <eehd80yul@gmail.com>
2024-12-20 14:58:56 +0900

    verticalSection

--- src/app/gsap/page.jsx
+++ src/app/gsap/page.jsx
@@ -3,11 +3,13 @@
 import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import HorizontalScrollSection from "./components/HorizontalScrollSection";
+import HorizontalScrollSection from "./components/horizontalScrollSection";
+import VerticalScrollSection from "./components/verticalScrollSection";
 
 export default function App() {
 
   return (
-    <HorizontalScrollSection />
+    // <HorizontalScrollSection />
+    <VerticalScrollSection />
   );
 }
